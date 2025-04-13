888ff8b0ff3f5f8621d60d27062e011ef1cd5124
Nick <nickcardosoo123@gmail.com>
2025-04-13 11:46:17 +0530

    randisk

--- main/data.php
+++ main/data.php
@@ -1,9 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $value = $_POST["value"] ?? "none";
-    file_put_contents("data.txt", $value);
-    echo "Received: $value";
-} else {
-    echo "Send a POST request with 'value'";
+if (isset($_POST['value'])) {
+    file_put_contents(__DIR__ . "/ramdisk/data.txt", $_POST['value'], LOCK_EX);
 }
 ?>
